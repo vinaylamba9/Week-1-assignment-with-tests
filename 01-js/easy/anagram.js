6aa616e40edad768b41d88aba6212058cd799f26
Vinay K <vinaylamba9@gmail.com>
2023-06-17 23:21:28 +0530

    anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,33 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length !== str2.length) return false;
+  let map = {};
+  str1.split('').forEach((element) => {
+    element = element.toLowerCase();
+    if (map[element]) {
+      map[element]++;
+    } else {
+      map[element] = 1;
+    }
+  });
+  let map2 = {};
+  str2.split('').forEach((element) => {
+    element = element.toLowerCase();
+    if (map2[element]) {
+      map2[element]++;
+    } else {
+      map2[element] = 1;
+    }
+  });
+  let res = true;
+  if (Object.keys(map).length !== Object.keys(map2).length) return false;
+  Object.keys(map).forEach((key) => {
+    if (map[key] !== map2[key]) {
+      res = false;
+    }
+  });
+  return res;
 }
 
 module.exports = isAnagram;
