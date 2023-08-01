1ea32f31cce9f950a3b0cadeb03bdc38ec06bd4a
Vinay K <vinaylamba9@gmail.com>
2023-08-01 12:35:45 -0400

    problems solved on class | Promises | async

--- 02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,13 @@
 */
 
 function wait(n) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('Promise Resolved after n seconds');
+    }, n * 1000);
+  });
 }
+
+wait(3)
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
