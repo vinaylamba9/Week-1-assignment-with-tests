ef206abdf1f9228cad73e3adee53b652d7b74677
Vinay K <vinaylamba9@gmail.com>
2023-08-02 09:34:24 -0400

    promise-chain | promise-all

--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -6,17 +6,54 @@
  */
 
 function waitOneSecond() {
-
+  return new Promise((res, rej) =>
+    setTimeout(() => res('resolved after 1 second'), 1000)
+  );
 }
 
 function waitTwoSecond() {
-
+  return new Promise((res, rej) =>
+    setTimeout(() => res('resolved after 2 second'), 2000)
+  );
 }
 
 function waitThreeSecond() {
-
+  return new Promise((res, rej) =>
+    setTimeout(() => res('resolved after 3 second'), 3000)
+  );
 }
 
-function calculateTime() {
+async function calculateTime() {
+  // USING ASYNC/AWAIT
 
+  //   try {
+  //     console.time();
+  //     const result1 = await waitOneSecond();
+  //     const result2 = await waitTwoSecond();
+  //     const result3 = await waitThreeSecond();
+  //     console.timeEnd();
+  //     console.log('Result 1:', result1);
+  //     console.log('Result 2:', result2);
+  //     console.log('Result 3:', result3);
+  //   } catch (error) {
+  //     console.error('Error occurred:', error);
+  //   }
+
+  // USING PROMISE CHAIN
+  console.time();
+  waitOneSecond()
+    .then((value1) => {
+      console.log(value1);
+      return waitTwoSecond();
+    })
+    .then((value2) => {
+      console.log(value2);
+      return waitThreeSecond();
+    })
+    .then((value3) => {
+      console.log(value3);
+      console.timeEnd();
+    });
 }
+
+calculateTime();
